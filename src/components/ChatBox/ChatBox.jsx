414c407f1b9dad90bb7cb828c970ac3645dd0bd3
vincenzo4002 <achieveraditya2@gmail.com>
2026-02-10 15:13:04 +0530

    commit right sidebar

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,32 @@ const ChatBox = () => {
                 <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
                 <img src={assets.help_icon} className='help' alt="" />
             </div>
+
+
+            <div className="chat-msg">
+                <div className="s-msg">
+                    <p className="msg">Lorem ipsum is placeholder text commonly used in ..</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+                <div className="s-msg">
+                    <img src={assets.pic1} className="msg-img" alt="" />
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+                <div className="r-msg">
+                    <p className="msg">Lorem ipsum is placeholder text commonly used in ..</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+            </div>
+
             <div className="chat-input">
                 <input type="text" placeholder='Send a message'/>
                 <input type="file" id='image' accept='image/png, image/jpeg' hidden />
